531a522d3113d8c5310bd7c5168be85ce9415e4f
Littlelinkrocks <77841378+Littlelinkrocks@users.noreply.github.com>
2021-02-14 22:28:24 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+
+for( i = 1000; i > 0; i-=2) {
+  console.log(i);
+}
+var i;
+for (i = 0;i <10001;) {
+}
+  if (i = 2500) {
+  console.log("your a quarter way there")  
+  } else if  (i = 5000) {
+     console.log (" halfway there")
+           
+  } if (i = 10000){
+    console.log ("you made it")
+
+  var Tvshows = ["dragon ball z", "one piece", " attack on titan", "jojo bizzare adevnture", "mob pyscho"]
+  for( var x = 0;  x < Tvshows.length; x++){
+     console.log(Tvshows[x]);
+  }
+}
